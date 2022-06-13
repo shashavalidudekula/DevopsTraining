5c7160390dc4067135207aa619ed4d481a77e9cb
Shashavali Dudekula <6346119+shashavalidudekula@users.noreply.github.com>
2022-06-13 12:00:30 +0530

    Update ques4.sh

--- interviewques/ques4.sh
+++ interviewques/ques4.sh
@@ -7,15 +7,15 @@
 echo
 read -p "Enter file/dir name: " name
 echo
-if [ -f $name ] 
+if [ -f $name ]          #checks given input is file or not
 then
 	cat $name 
 	exit 0
 fi
 
-if [ -d $name ]
+if [ -d $name ]        #checks given input is dir or not
 then
-	files=`find /home/shasha/shellscripts/interviewques/example/ -type f | wc -l`
+	files=`find /home/shasha/shellscripts/interviewques/example/ -type f | wc -l`      #ouputs the number of files in the given dir.. you can change the path according to your fiels
 	echo
 	echo "You have $files files in your $name dir"
 	echo
