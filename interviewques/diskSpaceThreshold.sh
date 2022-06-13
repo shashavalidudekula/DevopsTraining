83b28e5a1eb1b3e78eaf40b3c3224343ee13e651
Shashavali Dudekula <shashavali.d7869@gmail.com>
2022-06-13 15:33:22 +0530

    Update diskSpaceThreshold.sh

--- interviewques/diskSpaceThreshold.sh
+++ interviewques/diskSpaceThreshold.sh
@@ -6,13 +6,14 @@
 
 echo "Reading Disk usage of root "
 sleep 1
-x=`df | grep -i "/dev/nvme0n1p3" | awk '{print $5}' | cut -d % -f 1`
-echo -n "The current disk usage is at :" $x
+echo
+x=`df | grep -i "/dev/xvda2" | awk '{print $5}' | cut -d % -f 1`
 echo
 if [ $x -lt 85 ]
 then
-	echo "disk usage is $x"
+	echo "the current disk usage is at: $x %"
+    echo
 else
-	echo "disk usage is above 30%"
+	echo "disk usage is above 85%"
     mail -s "URGENT: Disk Threshold meets 85%" abc@xyz.com
 fi
