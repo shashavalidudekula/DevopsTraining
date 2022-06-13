1c64bc21ce1973e1b1be7476d24c00ed08305cd0
Shashavali Dudekula <shashavali.d7869@gmail.com>
2022-06-13 15:04:46 +0530

    new file

---
+++ interviewques/diskSpaceThreshold.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Purpose: Bash Scripting Training
+#Author : Shashavali
+#Date: 13th Jun,2022
+#use: Script to check the disk usage on server and email if disk usage is greaterthan 85%
+
+echo "Reading Disk usage of root "
+sleep
+x=`df | grep -i "/dev/nvme0n1p3" | awk '{print $5}' | cut -d % -f 1`
+echo -n "The current disk usage is at :" $x
+echo
+if [ $x -lt 85 ]
+then
+	echo "disk usage is $x"
+else
+	echo "disk usage is above 30%"
+    mail -s "URGENT: Disk Threshold meets 85%" abc@xyz.com
+fi
