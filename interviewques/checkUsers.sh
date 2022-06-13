e08b410318830efb03212576e4f57aa02e06112b
Shashavali Dudekula <shashavali.d7869@gmail.com>
2022-06-13 12:54:47 +0530

    Create checkUsers.sh

---
+++ interviewques/checkUsers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Purpose: Bash Scripting Training
+#Author : Shashavali
+#Date: 13th Jun,2022
+#use: Script for checking given users are present or not
+
+echo
+for i in $@                #passing command line args to for loog using $@
+do
+        grep -i $i /etc/passwd > /dev/null    #check users are present in /etc/passwd (users file) and curbing the output to null.
+        if [ $? -eq 0 ]
+        then
+                echo "$i - Present"
+        else
+                echo "$i - Not present"
+        fi
+done
+echo
