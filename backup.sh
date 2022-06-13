2300b2c3ddb8a68bccaf1e39eb4c62d56a7fd09c
Shashavali Dudekula <6346119+shashavalidudekula@users.noreply.github.com>
2022-06-13 18:00:17 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@
 #Date: 13th June,2022
 #Use: creates a backup of shellscripts dir in root home dir
 
-tar -cvf /tmp/backup.tar /home/shasha/shellscripts      #tar the files you want to backup
-gzip /tmp/backup.tar    #compress the tar again to save space
+tar -cvf /tmp/backup.tar /home/shasha/shellscripts                          #tar the files you want to backup
+gzip /tmp/backup.tar                                                        #compress the tar again to save space
 if [ $? -eq 0 ]
 then
 	echo " Backup created successfully"\
